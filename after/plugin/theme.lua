9954e85652c2ee15367e3f18809d77a8f82c7e8d
morten.manzelmann <morten@manzelmann.org>
2023-02-10 20:03:54 +0100

    Add indentation lines

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -9,4 +9,4 @@ require("lualine").setup({ options = { theme = "nightfox" } })
 
 vim.cmd("colorscheme nightfox")
 vim.cmd("highlight IndentBlanklineChar guifg=#3E4452 gui=nocombine")
-vim.cmd("highlight IndentBlanklineContextChar guifg=#222222 gui=nocombine")
+vim.cmd("highlight IndentBlanklineContextChar guifg=#dddddd gui=nocombine")
